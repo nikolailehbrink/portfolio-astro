9c1f973f2ad31ac91825393566dfbca21f6eb8ba
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-09-07 19:49:25 +0200

    feat: update site url

--- astro.config.js
+++ astro.config.js
@@ -14,7 +14,8 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: (process.env.VERCEL_PROJECT_PRODUCTION_URL ??=
+    "https://www.nikolailehbr.ink"),
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
