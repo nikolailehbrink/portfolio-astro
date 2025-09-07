87e4fa9851fa8155bd87fdd484a81d9fa4ec1f6d
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-09-07 13:15:48 +0200

    feat: update blog post metadata and add authors collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, reference, z } from 'astro:content';
+import { file, glob } from 'astro/loaders';
 
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -10,10 +10,29 @@ const blog = defineCollection({
 			title: z.string(),
 			description: z.string(),
 			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			publicationDate: z.coerce.date(),
+			modificationDate: z.coerce.date().optional(),
+			cover: image().optional(),
+			tags: z.array(z.string().min(1)).optional(),
+			authors: z.array(reference("authors")).min(1).default(['nikolailehbrink']),
 		}),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+	loader: file("src/data/authors/authors.json"), schema: ({image}) => z.object({
+		name: z.string(),
+		image: image().optional(),
+		x: z.string().url().refine(arg => arg.includes("x.com"), {
+  message: "URL must contain x.com"
+}).optional(),
+github: z.string().url().refine(arg => arg.includes("github.com"), {
+  message: "URL must contain github.com"
+}).optional(),
+linkedin: z.string().url().refine(arg => arg.includes("linkedin.com"), {
+	message: "URL must contain linkedin.com"
+}).optional(),
+}),
+
+})
+
+export const collections = { blog, authors };
