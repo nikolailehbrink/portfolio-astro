8ba7d3233b06b9ed890a46f4f8df8b698cb6288d
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-09-07 13:15:58 +0200

    feat: add Prettier configuration with Tailwind CSS support

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+const functions = ["cn", "clsx", "cva", "tw"];
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  tailwindFunctions: functions,
+  tailwindAttributes: ["tw"],
+  customFunctions: functions,
+  tailwindStylesheet: "./src/styles/global.css",
+  endingPosition: "absolute",
+  plugins: [
+    "prettier-plugin-astro",
+    // https://www.nikolailehbr.ink/blog/tailwind-css-tips#Automatic-wrapping-of-long-class-names
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-classnames",
+    "prettier-plugin-merge",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+};
