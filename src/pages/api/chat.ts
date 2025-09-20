83af932b492bb51ecaa1cc2d3e7f0062db991d3f
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-09-20 21:29:37 +0200

    feat: implement message limit and cookie management for chat component

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,5 +1,6 @@
 export const prerender = false;
 
+import { AI_CHAT_MESSAGE_LIMIT, SECONDS_TO_CHAT_AGAIN } from "@/consts";
 import { createOpenAI } from "@ai-sdk/openai";
 import { streamText, type UIMessage, convertToModelMessages } from "ai";
 import type { APIRoute } from "astro";
@@ -11,12 +12,26 @@ const openai = createOpenAI({
   apiKey: import.meta.env.OPENAI_API_KEY,
 });
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, cookies }) => {
   const { messages }: { messages: UIMessage[] } = await request.json();
+  const messageCount = cookies.get("message_count")?.number();
+
+  if (messageCount !== undefined && messageCount >= AI_CHAT_MESSAGE_LIMIT) {
+    return new Response("Message limit reached", { status: 403 });
+  }
 
   const result = streamText({
     model: openai("gpt-4o"),
     messages: convertToModelMessages(messages),
+  });
+
+  cookies.set("message_count", String(messageCount ? messageCount + 1 : 1), {
+    // expires: new Date(Date.now() + 100000000),
+    maxAge: SECONDS_TO_CHAT_AGAIN,
+    httpOnly: true,
+    // secure: import.meta.env.PROD,
+    sameSite: "lax",
+    path: "/",
   });
 
   return result.toUIMessageStreamResponse();
