88a96648ed3704eac1e17bb9bfca52b427e19037
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-10-25 13:38:09 +0200

    feat: enhance ImageZoom component with custom zoom icon

--- src/components/react/ImageZoom.tsx
+++ src/components/react/ImageZoom.tsx
@@ -3,6 +3,7 @@ import Zoom, {
   type UncontrolledProps,
 } from 'react-medium-image-zoom';
 import { cn } from '@/lib/utils';
+import { MagnifyingGlassPlusIcon } from '@phosphor-icons/react/dist/ssr/MagnifyingGlassPlus';
 
 export type ImageZoomProps = UncontrolledProps & {
   isZoomed?: ControlledProps['isZoomed'];
@@ -19,11 +20,7 @@ export default function ImageZoom({
       className={cn(
         'relative',
         '[&_[data-rmiz-ghost]]:pointer-events-none [&_[data-rmiz-ghost]]:absolute',
-        '[&_[data-rmiz-btn-zoom]]:m-0 [&_[data-rmiz-btn-zoom]]:size-10 [&_[data-rmiz-btn-zoom]]:touch-manipulation [&_[data-rmiz-btn-zoom]]:appearance-none [&_[data-rmiz-btn-zoom]]:rounded-[50%] [&_[data-rmiz-btn-zoom]]:border-none [&_[data-rmiz-btn-zoom]]:bg-foreground/70 [&_[data-rmiz-btn-zoom]]:p-2 [&_[data-rmiz-btn-zoom]]:text-background [&_[data-rmiz-btn-zoom]]:outline-offset-2',
-        '[&_[data-rmiz-btn-unzoom]]:m-0 [&_[data-rmiz-btn-unzoom]]:size-10 [&_[data-rmiz-btn-unzoom]]:touch-manipulation [&_[data-rmiz-btn-unzoom]]:appearance-none [&_[data-rmiz-btn-unzoom]]:rounded-[50%] [&_[data-rmiz-btn-unzoom]]:border-none [&_[data-rmiz-btn-unzoom]]:bg-foreground/70 [&_[data-rmiz-btn-unzoom]]:p-2 [&_[data-rmiz-btn-unzoom]]:text-background [&_[data-rmiz-btn-unzoom]]:outline-offset-2',
-        '[&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:pointer-events-none [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:absolute [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:size-px [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:overflow-hidden [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:whitespace-nowrap [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:[clip-path:inset(50%)] [&_[data-rmiz-btn-zoom]:not(:focus):not(:active)]:[clip:rect(0_0_0_0)]',
-        '[&_[data-rmiz-btn-zoom]]:absolute [&_[data-rmiz-btn-zoom]]:top-2.5 [&_[data-rmiz-btn-zoom]]:right-2.5 [&_[data-rmiz-btn-zoom]]:bottom-auto [&_[data-rmiz-btn-zoom]]:left-auto [&_[data-rmiz-btn-zoom]]:cursor-zoom-in',
-        '[&_[data-rmiz-btn-unzoom]]:absolute [&_[data-rmiz-btn-unzoom]]:top-5 [&_[data-rmiz-btn-unzoom]]:right-5 [&_[data-rmiz-btn-unzoom]]:bottom-auto [&_[data-rmiz-btn-unzoom]]:left-auto [&_[data-rmiz-btn-unzoom]]:z-[1] [&_[data-rmiz-btn-unzoom]]:cursor-zoom-out',
+        '[&_[data-rmiz-btn-zoom]]:m-0 [&_[data-rmiz-btn-zoom]]:touch-manipulation [&_[data-rmiz-btn-zoom]]:bg-foreground/60 [&_[data-rmiz-btn-zoom]]:backdrop-blur-lg [&_[data-rmiz-btn-zoom]]:rounded-xl [&_[data-rmiz-btn-zoom]]:p-1.5 [&_[data-rmiz-btn-zoom]]:text-background [&_[data-rmiz-btn-zoom]]:absolute [&_[data-rmiz-btn-zoom]]:bottom-0 [&_[data-rmiz-btn-zoom]]:right-0 [&_[data-rmiz-btn-zoom]]:rounded-tr-none [&_[data-rmiz-btn-zoom]]:rounded-bl-none',
         '[&_[data-rmiz-content="found"]_img]:cursor-zoom-in',
         '[&_[data-rmiz-content="found"]_svg]:cursor-zoom-in',
         '[&_[data-rmiz-content="found"]_[role="img"]]:cursor-zoom-in',
@@ -43,6 +40,7 @@ export default function ImageZoom({
           'motion-reduce:[&_[data-rmiz-modal-img]]:transition-none motion-reduce:[&_[data-rmiz-modal-overlay]]:transition-none',
           backdropClassName
         )}
+        IconZoom={()=><MagnifyingGlassPlusIcon size={20} weight='duotone'  />}
         {...props} />
     </div>
   );
