485cb53209876280139b7f173e6404b802c4f24a
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-10-30 23:25:30 +0100

    refactor: remove unnecessary llamaindex package and readd sharp

--- knip.config.ts
+++ knip.config.ts
@@ -10,15 +10,15 @@ export default {
   },
   ignoreBinaries: ["dotenv"],
   ignoreFiles: ["src/layouts/LegalLayout.astro"],
-  // llamaindex/vercel throws error without llamaindex package
   // @react-email/preview-server is used for email previews only
   // @typescript-eslint/parser is used to make ESLint VSCode Extension work in Astro files: https://github.com/ota-meshi/eslint-plugin-astro?tab=readme-ov-file#-installation
   // @astrojs/check is used for Astro type checking
+  // sharp is used for image processing in local builds
   ignoreDependencies: [
     "@react-email/preview-server",
-    "llamaindex",
     "@typescript-eslint/parser",
     "@astrojs/check",
+    "sharp",
   ],
   // https://github.com/webpro-nl/knip/issues/1149#issuecomment-2994091874
   commitlint: true,
